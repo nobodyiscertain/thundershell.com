2a29c1ba550354d82bd5bbc9ab59a4206a3916d2
Jamie Wagner <jamie@kajabi.com>
2020-08-08 03:10:22 -0700

    Move site

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+  <title>Thundershell :: Coming Soon</title>
+  <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+  <link href="http://fonts.googleapis.com/css?family=Oswald&v1" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="/css/styles.css" type="text/css" />
+  <!--[if IE]>
+    <link rel="stylesheet" href="/css/ie.css" type="text/css" />
+  <![endif]-->
+  <!--[if lte IE 7]>
+    <link rel="stylesheet" href="/css/ie7.css" type="text/css" />
+  <![endif]-->
+  <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-2019202-22']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script>
+</head>
+
+<body>
+  <div id="container">
+    <div id="header">
+      <h1>Thundershell</h1>
+    </div><!-- /header -->
+    <div id="content">
+      <img src="images/helmet2.png" id="helmet" />
+      <div id="right-column">
+        <h2>So you like to play kart, do you?</h2>
+        <p>Well, have we got a surprise in store for you. We&rsquo;re not ready to pull back the curtains just yet, but we&rsquo;ve got our golden mushroom and we&rsquo;re headed straight for the finish line. So how about you type out your email address right here, and we&rsquo;ll let you know when the party is starting.</p>
+        <form action="http://lateporter.createsend.com/t/r/s/qjhtut/" method="post" id="subForm">
+          <div>
+            <input type="text" name="cm-qjhtut-qjhtut" id="qjhtut-qjhtut" />
+            <input type="submit" value="Subscribe" id="newsletter-submit" />
+          </div>
+        </form>
+      </div>
+    </div><!-- /content -->
+    <div id="footer">
+      <p>&copy; 2009 - <?php print date('Y'); ?> Thundershell, LLC.</p>
+    </div>
+  </div>
+</body>
+</html>
---
+++ thanks.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+  <title>Thundershell :: Coming Soon</title>
+  <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+  <link href="http://fonts.googleapis.com/css?family=Anton&v1" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="/css/styles.css" type="text/css" />
+  <!--[if IE]>
+    <link rel="stylesheet" href="/css/ie.css" type="text/css" />
+  <![endif]-->
+  <!--[if lte IE 7]>
+    <link rel="stylesheet" href="/css/ie7.css" type="text/css" />
+  <![endif]-->
+  <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-2019202-22']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script>
+</head>
+
+<body>
+  <div id="container">
+    <div id="header">
+      <h1>Thundershell</h1>
+    </div><!-- /header -->
+    <div id="content">
+      <img src="images/helmet.png" id="helmet" />
+      <div id="right-column">
+        <h2>You Did It!</h2>
+        <p>Thanks. We&rsquo;ll see you soon, hot shot. In the meantime it might be a good idea to brush up on your skills. You&rsquo;re looking pretty sloppy out there.</p>
+      </div>
+    </div><!-- /content -->
+    <div id="footer">
+      <p>&copy; 2009 - <?php print date('Y'); ?> Thundershell, LLC.</p>
+    </div>
+  </div>
+</body>
+</html>
